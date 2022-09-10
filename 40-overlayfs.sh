737518343b514bd96833c4df51847ec0786dabc9
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2022-09-10 22:37:30 +0200

    wip

--- 40-overlayfs.sh
+++ 40-overlayfs.sh
@@ -4,4 +4,8 @@ set -eux
 
 sudo mkdir -p /upper /lower /work /merged
 sudo chmod 777 /upper /lower /work /merged
+echo 'upper foo' > /upper/foo
+echo 'upper bar' > /upper/bar
+echo 'lower bar' > /lower/bar
+echo 'lower quux' > /lower/quux
 sudo mount -t overlay overlay -olowerdir=/lower,upperdir=/upper,workdir=/work /merged 
