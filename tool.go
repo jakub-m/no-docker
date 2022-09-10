95f6446754d51a205f68f936775cf4c66542b33a
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2022-09-10 07:42:23 +0200

    tool

--- tool.go
+++ tool.go
@@ -3,8 +3,11 @@ package main
 import (
 	"flag"
 	"log"
+	"runtime"
 	"time"
 )
+
+const mb = 1024 * 1024
 
 func main() {
 	var memoryMb int
@@ -13,16 +16,15 @@ func main() {
 	flag.StringVar(&hangWithLabel, "hang", "", "hang on!")
 	flag.Parse()
 	if memoryMb > 0 {
-		log.Printf("allocate %dMB of memory", memoryMb)
-		size := memoryMb * 1024 * 1024
-		block := make([]uint8, size, size)
-		// do something with memory to ensure that this code is not optimized-out.
-		n := 0
-		for i := 0; i < cap(block); i++ {
-			block[i] = 1
-			n++
+		for {
+			allocate(memoryMb)
+			readMemStats()
+			log.Printf("run garbage collector")
+			time.Sleep(1 * time.Second)
+			runtime.GC()
+			readMemStats()
+			time.Sleep(1 * time.Second)
 		}
-		log.Printf("allocated %d bytes", n)
 	}
 	if hangWithLabel != "" {
 		log.Print("hang on!")
@@ -30,4 +32,23 @@ func main() {
 			time.Sleep(1 * time.Second)
 		}
 	}
+}
+
+func allocate(memoryMb int) {
+	log.Printf("allocate %dMB of memory", memoryMb)
+	size := memoryMb * 1024 * 1024
+	block := make([]uint8, size, size)
+	// do something with memory to ensure that this code is not optimized-out.
+	n := 0
+	for i := 0; i < cap(block); i++ {
+		block[i] = 1
+		n++
+	}
+	log.Printf("allocated %d bytes", n)
+}
+
+func readMemStats() {
+	stats := runtime.MemStats{}
+	runtime.ReadMemStats(&stats)
+	log.Printf("heap %d mb, sys %d mb", stats.HeapAlloc/mb, stats.Sys/mb)
 }
